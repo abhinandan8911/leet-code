1cb2d66e564f331a2fea12c94fb93a08af90c94a
Abhinandan Bhattacharya <abhinandan.bhattacharya@gmail.com>
2021-11-23 20:06:53 -0600

    solution

---
+++ src/main/java/com/abhi/tutorial/MoveZeroes.java
@@ -0,0 +1,26 @@
+package com.abhi.tutorial;
+
+import java.util.Arrays;
+
+public class MoveZeroes {
+    public static int[] moveZeroes(int[] nums) {
+        int[] temp = new int[nums.length];
+        Arrays.fill(temp, 0);
+        for (int i = 0, j = 0; i < temp.length && j < nums.length; j++) {
+            if (nums[j] != 0) {
+                temp[i] = nums[j];
+                i++;
+            }
+        }
+        nums = temp;
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {0,1,0,3,12};
+        int[] result = moveZeroes(input);
+        for (int j : result) {
+            System.out.println(j);
+        }
+    }
+}
