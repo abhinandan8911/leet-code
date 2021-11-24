9ba65c33ce737c3346aa9692c46594f92a2da5aa
Abhinandan Bhattacharya <abhinandan.bhattacharya@gmail.com>
2021-11-23 19:43:33 -0600

    solution

---
+++ src/main/java/com/abhi/tutorial/MergeSortedArrays.java
@@ -0,0 +1,13 @@
+package com.abhi.tutorial;
+
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+    public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m, j = 0; i < nums1.length && j < nums2.length; i++, j++) {
+            nums1[i] = nums2[j];
+        }
+        Arrays.sort(nums1);
+        return nums1;
+    }
+}
