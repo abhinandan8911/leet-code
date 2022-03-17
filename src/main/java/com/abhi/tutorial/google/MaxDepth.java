00b2d38e1bb2a571bafb69780f3281f113fc1e58
Abhinandan Bhattacharya <abhinandan.bhattacharya@gmail.com>
2022-03-17 16:35:17 -0500

    solution
    https://leetcode.com/explore/learn/card/data-structure-tree/17/solve-problems-recursively/535/

---
+++ src/main/java/com/abhi/tutorial/google/MaxDepth.java
@@ -0,0 +1,18 @@
+package com.abhi.tutorial.google;
+
+// https://leetcode.com/explore/learn/card/data-structure-tree/17/solve-problems-recursively/535/
+
+import com.abhi.tutorial.google.model.TreeNode;
+
+public class MaxDepth {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int maxLeftDepth = maxDepth(root.left);
+        int maxRightDepth = maxDepth(root.right);
+        return Math.max(maxLeftDepth, maxRightDepth) + 1;
+    }
+
+}
