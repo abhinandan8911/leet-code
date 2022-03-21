2ffdddc46ae0bc9adc1bee920882bb77376a225d
abhi bhattacharya <abhinandan.bhattacharya@gmail.com>
2022-03-21 08:39:36 -0500

    done

--- src/main/java/com/abhi/tutorial/scratch/PrimeNumbers.java
+++ src/main/java/com/abhi/tutorial/scratch/PrimeNumbers.java
@@ -14,9 +14,18 @@ public class PrimeNumbers {
         return primes.stream().mapToInt(Integer::intValue).toArray();
     }
 
-    private static boolean isPrime(int num) {
+    public static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        if (num == 2) {
+            return true;
+        }
+        if (num % 2 == 0) {
+            return false;
+        }
         double limit = Math.sqrt(num);
-        for (int i = 2; i <= limit; i++) {
+        for (int i = 3; i <= limit; i+=2) {
             if (num % i == 0) {
                 return false;
             }
@@ -25,7 +34,7 @@ public class PrimeNumbers {
     }
 
     public static void main(String[] args) {
-        int[] result = listOfPrimesTillNumber(1000000);
+        int[] result = listOfPrimesTillNumber(20);
         for (int num : result) {
             System.out.println(num);
         }
