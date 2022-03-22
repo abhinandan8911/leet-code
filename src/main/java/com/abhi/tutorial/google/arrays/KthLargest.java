8e91d0297bc059555b33ea333adf45e94a186468
Abhinandan Bhattacharya <abhinandan.bhattacharya@gmail.com>
2022-03-22 16:44:08 -0500

    solution
    https://leetcode.com/explore/interview/card/google/59/array-and-strings/361/

---
+++ src/main/java/com/abhi/tutorial/google/arrays/KthLargest.java
@@ -0,0 +1,19 @@
+package com.abhi.tutorial.google.arrays;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+
+// https://leetcode.com/explore/interview/card/google/59/array-and-strings/361/
+
+public class KthLargest {
+    public int findKthLargest(int[] nums, int k) {
+        int[] sorted = Arrays.stream(nums).boxed().sorted(Collections.reverseOrder()).mapToInt(Integer::intValue).toArray();
+        return sorted[k-1];
+    }
+
+    public static void main(String[] args) {
+        int[] input = {3,2,1,5,6,4};
+        System.out.println((new KthLargest()).findKthLargest(input, 2));
+    }
+}
