aeb1aac24f8cdff9f14e96c81b24935884229909
Abhinandan Bhattacharya <abhinandan.bhattacharya@gmail.com>
2022-04-07 20:10:47 -0500

    solution

--- src/main/java/com/abhi/tutorial/tree/LowestCommonAncestor.java
+++ src/main/java/com/abhi/tutorial/tree/LowestCommonAncestor.java
@@ -1,25 +1,38 @@
 package com.abhi.tutorial.tree;
 
+
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+import com.abhi.tutorial.model.TreeNode;
+
 public class LowestCommonAncestor {
 
-//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-//        TreeNode closestP = matchingNode(root, p);
-//        TreeNode closestQ = matchingNode(root, q);
-//        while ((closestP != null && closestQ != null) && (closestP != closestQ)) {
-//            closestP = matchingNode(root, closestP);
-//            closestQ = matchingNode(root, closestQ);
-//        }
-//        return closestP;
-//    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (isTreeParent(root, p) && isTreeParent(root, q)) {
+            TreeNode left = root.left;
+            TreeNode right = root.right;
+            if (isTreeParent(left, p) && isTreeParent(left, q)) {
+                lowestCommonAncestor(left, p, q);
+            }
+            if (isTreeParent(right, p) && isTreeParent(right, q)) {
+                lowestCommonAncestor(right, p, q);
+            }
+        }
+        return root;
+    }
 
-//    private TreeNode matchingNode(TreeNode root, TreeNode target) {
-//        if (root == null) {
-//            return null;
-//        }
-//
-//        TreeNode
-//
-//
-//    }
+    private boolean isTreeParent(TreeNode root, TreeNode child) {
+        if (root == null) {
+            return false;
+        }
 
+        if (root.val == child.val) {
+            return true;
+        }
+
+        return isTreeParent(root.left, child) || isTreeParent(root.right, child);
+    }
 }
