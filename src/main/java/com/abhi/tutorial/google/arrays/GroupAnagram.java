7a2b63b771d8e077437f0520dbce5af0765f21d9
Abhinandan Bhattacharya <abhinandan.bhattacharya@gmail.com>
2022-04-09 14:53:09 -0500

    solution

--- src/main/java/com/abhi/tutorial/google/arrays/GroupAnagram.java
+++ src/main/java/com/abhi/tutorial/google/arrays/GroupAnagram.java
@@ -7,35 +7,15 @@ import java.util.*;
 public class GroupAnagram {
 
     public List<List<String>> groupAnagrams(String[] strs) {
-        Set<String> visited = new HashSet<>();
-        List<List<String>> result = new ArrayList<>();
-        for (int i = 0; i < strs.length && visited.size() < strs.length; i++) {
-            String first = strs[i];
-            if (!visited.contains(first)) {
-                List<String> list = new ArrayList<>();
-                list.add(first);
-                visited.add(first);
-                for (int j = i + 1; j < strs.length; j++) {
-                    String next = strs[j];
-                    if (!visited.contains(next) && isAnagram(first, next)) {
-                        visited.add(next);
-                        list.add(next);
-                    }
-                }
-                result.add(list);
-            }
+        if (strs.length == 0) {
+            return new ArrayList<>();
         }
-        return result;
-    }
-
-    private boolean isAnagram(String a, String b) {
-        if (a.length() != b.length() || (a.length() == 0 && b.length() == 0)) {
-            return false;
+        Map<String, List<String>> ans = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            ans.computeIfAbsent(String.valueOf(chars), s -> new ArrayList<>()).add(str);
         }
-        char[] achars = a.toCharArray();
-        char[] bchars = b.toCharArray();
-        Arrays.sort(achars);
-        Arrays.sort(bchars);
-        return Arrays.equals(achars, bchars);
+        return new ArrayList<>(ans.values());
     }
 }
